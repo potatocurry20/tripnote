7935b8b88fa58acd64e354ed1451f6fb8f07723b
”名前” <greatpotato20@gmail.com>
2019-08-31 10:27:59 +0000

    0831

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
-class ApplicationController < ActionController::Base
-  before_action :current_user, :logged_in?, :log_out
-  include SessionsHelper
+module SessionsHelper
+  
+  #ログイン時にセッションのユーザーIDにログインユーザーのIDを代入する
+  def login(user)
+    session[:user_id] = user.id
+  end
   
   #ログインユーザーがいる場合、現在のユーザーにログインユーザーを代入する
   def current_user
@@ -14,4 +17,9 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
   
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #remember_tokenというインスタンスをuserモデルに持たせる
+  attr_accessor :remember_token
   before_save{self.email = email.downcase}
   
   validates :name, presence: true, length: {maximum: 15}
@@ -9,5 +11,22 @@ class User < ApplicationRecord
   has_secure_password
   VALID_PASWAD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
   validates :password, presence: true, length: {in: 8..32}, format: {with: VALID_PASWAD_REGEX}
+  
+  #渡された文字列のハッシュを返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost? Bcrypt::Engine::MIN_COST :Bcrypt::Engine.cost
+    Bcrypt::Password.create(string, cost: cost)
+  end
+  
+  #ランダムなトークンを作成する
+  #def User.new_token
+    #SecureRandom.urlsafe_base64
+  #end
+  
+  #selfにトークンを追加し、remenber_digestをremenber_tokenで更新する
+  #def remenber
+    #self.remember_token = user.new_token
+    #update_attribute(:remenber_digest, User.digest(remenber_token))
+  #end
   
 end
