2519d16a0e390b782f1661dc35553b5d1815ea3e
”名前” <greatpotato20@gmail.com>
2019-09-22 03:52:20 +0000

    0922

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -12,10 +12,29 @@ class PlansController < ApplicationController
     @plan = current_user.plans.new(plan_params)
     
     if @plan.save
+      flash[:success]="You created the new plan."
       redirect_to plans_path
     else
       render :new
     end
+  end
+  
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+  
+  def update
+    @plan = Plan.find(params[:id])
+    if @plan.update_attributes(plan_params)
+      redirect_to plans_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Plan.find(params[:id]).destroy
+    redirect_to plans_path
   end
   
   
